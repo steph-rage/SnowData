0c6a191c4937ecde9ce076a4d58ec5936bfabc7a
steph-rage <stephlage621@gmail.com>
2017-02-06 17:01:33 -0500

    Now gets data from all years since 1980, saves to database

--- snow_data.py
+++ snow_data.py
@@ -4,33 +4,25 @@ import sqlite3
 from bs4 import BeautifulSoup
 
 #NOTE: for some reason the year is off by one. Putting '1981' in the POST data gives 
-#values from 1980.  
-
-page = []
-
-
-#Year counts the number of years since 1981, the first available year with data
-#for year in range(1981, 2017):
-#
-#	POST_data = {'sitenum':'515', 'report':'STAND', 'timeseries':'Hourly', 'interval':'YEAR='+str(year), 'temp_unit':'8', 'format':'view', 'autoscale':'false', 'legendpos':'right', 'userEmail':'', 'intervalDirection':'-'}
-#	page.append(requests.post('https://wcc.sc.egov.usda.gov/nwcc/view', POST_data))
-
-POST_data = {'sitenum':'515', 'report':'STAND', 'timeseries':'Hourly', 'interval':'YEAR=1981', 'temp_unit':'8', 'format':'view', 'autoscale':'false', 'legendpos':'right', 'userEmail':'', 'intervalDirection':'-'}
-page = requests.post('https://wcc.sc.egov.usda.gov/nwcc/view', POST_data)
-
-
-soup = BeautifulSoup(page.text, "html5lib")
-table_cells = soup.find_all('td')
+#values from 1980.
 
 
 snow_db = sqlite3.connect('snow-database.db')
 snow_db.execute('CREATE TABLE snow_level (DAY date, SNOW_LEVEL int)')
 
-snow_db.execute('INSERT INTO snow_level VALUES (01-31-2017, 5)')
 
-for i in range(len(table_cells)):
-	if '1980' in table_cells[i].text:
-		snow_db.execute('INSERT INTO snow_level VALUES (?, ?)', (table_cells[i].text, table_cells[i+3].text))
+for year in range(1980, 2017):
+
+	POST_data = {'sitenum':'515', 'report':'STAND', 'timeseries':'Hourly', 'interval':'YEAR='+str(year), 'temp_unit':'8', 'format':'view', 'autoscale':'false', 'legendpos':'right', 'userEmail':'', 'intervalDirection':'-'}
+	page = requests.post('https://wcc.sc.egov.usda.gov/nwcc/view', POST_data)
+
+	soup = BeautifulSoup(page.text, "html5lib")
+	table_cells = soup.find_all('td')
+
+	for i in range(len(table_cells)):
+		if str(year - 1) in table_cells[i].text:
+			snow_db.execute('INSERT INTO snow_level VALUES (?, ?)', (table_cells[i].text, table_cells[i+3].text))
+
 
 snow_db.commit()
 snow_db.close()
